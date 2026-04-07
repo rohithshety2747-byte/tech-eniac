5435e2983e65c9f3b0cb0bb86d74123f2fe6e768
rohithshety2747-byte <rohithshety2747@gmail.com>
2026-04-07 13:22:17 +0530

    v2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { connection } from "next/server";
 import { HomePage } from "@/components/HomePage";
 
 async function wait(ms: number) {
@@ -7,6 +8,7 @@ async function wait(ms: number) {
 }
 
 export default async function Home() {
+  await connection();
   await wait(4000);
 
   return <HomePage />;
